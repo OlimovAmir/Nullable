4313054f3c8c856ccb502c62c30474e21e2f5581
olimov Amir <olimov.amir@mail.ru>
2023-12-11 16:52:08 +0300

    fix bag

--- Program.cs
+++ Program.cs
@@ -5,8 +5,24 @@ class Program
     {
         string str = null;
         
-        int a = null;
+        int? a = null;
 
-        DateTime dateTime = null;
+        DateTime? dateTime = null;
+
+
+        Console.WriteLine(a == null); // True 
+        Console.WriteLine(a.HasValue); // False
+        Console.WriteLine(a.GetValueOrDefault()); // 0
+        Console.WriteLine(a.GetValueOrDefault(3)); // 3
+        Console.WriteLine(a ?? 55); // 55
+
+        try
+        {
+            Console.WriteLine(a.Value); // InvalidOperationException
+        }catch (Exception ex)
+        {
+            Console.WriteLine("no Data");
+        }
+        
     }
 }
